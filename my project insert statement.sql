f2ccd9c95c5544e52a4f3a13bb0191056311f2d3
Kwame <144356579+K12Boateng@users.noreply.github.com>
2024-05-09 16:04:16 +0000

    Add files via upload
    Insertion

---
+++ my project insert statement.sql
@@ -0,0 +1,5 @@
+insert into students (student_id,first_name,last_name,Hometown)
+values(1,'Emmanuel','Amoako','Ada')
+       (2,'Priscilla','Badu','Koforidua')
+	   (3,'Emily','Koomson','Cape Coast')
+	   
